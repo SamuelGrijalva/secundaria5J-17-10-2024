4dd37ebf28a84822da4044d175685453394687e5
SamuelGrijalva <a.22308051281205@cbtis128.edu.mx>
2024-10-22 19:29:18 -0600

    Add files via upload

---
+++ grupo5JV/appsecundaria/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import AlumnoT, Frase
+
+# Register your models here.
+
+
+class ComentarioIntLine(admin.TabularInline):
+    model=Frase
+    extra=1
+
+class AlumnoAdmin(admin.ModelAdmin):
+    fields=["apaterno", "amaterno", "nombre", "fecha_nacimiento", "fecha_ingreso"]
+    list_display=["apaterno", "amaterno", "nombre"]
+    inlines=[ComentarioIntLine]
+
+admin.site.register(AlumnoT, AlumnoAdmin)
+
+@admin.register(Frase)
+class FraseAdmin(admin.ModelAdmin):
+    fields=["comentario","Alumno_fk"]
+    list_display=["comentario"]
